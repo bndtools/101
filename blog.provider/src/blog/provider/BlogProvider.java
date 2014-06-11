b87a15bdd0f5fb12f7053ce1711138df3eb43a8c
Neil Bartlett <njbartlett@gmail.com>
2014-06-11 03:41:57 +0100

    Updated blog.provider to MongoDB

--- blog.provider/src/blog/provider/BlogProvider.java
+++ blog.provider/src/blog/provider/BlogProvider.java
@@ -1,23 +1,49 @@
 package blog.provider;
 
+import java.security.MessageDigest;
+import java.util.ArrayList;
 import java.util.List;
 
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
 import blog.api.Blog;
-import aQute.bnd.annotation.component.*;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 
 @Component
 public class BlogProvider implements Blog {
+	
+	private DB db;
+	private MessageDigest digest;
+
+	@Activate
+	void activate() throws Exception {
+		MongoClient client = new MongoClient("localhost");
+		db = client.getDB("blog");
+		
+		digest = MessageDigest.getInstance("SHA-256");
+	}
 
 	@Override
 	public List<String> listEntries() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		DBCollection entries = db.getCollection("entries");
+		List<String> result = new ArrayList<String>((int) entries.count());
+		for (DBObject obj : entries.find()) {
+			result.add((String) obj.get("text"));
+		}
+		return result;
 	}
 
 	@Override
 	public void addEntry(String content) throws Exception {
-		// TODO Auto-generated method stub
-		
+		DBCollection entries = db.getCollection("entries");
+		String hash = new String(digest.digest(content.getBytes()));
+		BasicDBObject obj = new BasicDBObject("hash", hash).append("text", content);
+		entries.insert(obj);
 	}
 
 }
