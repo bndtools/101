aaafcd8387b859313d16678738ff606dc8205b8b
Neil Bartlett <njbartlett@gmail.com>
2014-06-11 04:07:06 +0100

    Make blog.consumer a servlet

--- blog.consumer/src/blog/consumer/BlogConsumer.java
+++ blog.consumer/src/blog/consumer/BlogConsumer.java
@@ -1,10 +1,31 @@
 package blog.consumer;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import aQute.bnd.annotation.component.*;
 
-@Component
-public class BlogConsumer {
+@Component(
+		properties = {
+				"alias=/blog"
+		})
+public class BlogConsumer extends HttpServlet implements Servlet {
 
-	// TODO: class provided by template
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setHeader("Content-Type", "text/plain");
+		PrintWriter writer = resp.getWriter();
+		writer.println("Hello there");
+		writer.flush();
+		writer.close();
+	}
 
 }
