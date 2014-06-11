b2d53b91116f06561bd82069efafd0c857fd8fbe
Neil Bartlett <njbartlett@gmail.com>
2014-06-11 04:17:08 +0100

    Output blog content in plain text

--- blog.consumer/src/blog/consumer/BlogConsumer.java
+++ blog.consumer/src/blog/consumer/BlogConsumer.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import blog.api.Blog;
 import aQute.bnd.annotation.component.*;
 
 @Component(
@@ -18,12 +19,27 @@ import aQute.bnd.annotation.component.*;
 public class BlogConsumer extends HttpServlet implements Servlet {
 
 	private static final long serialVersionUID = 1L;
+	private Blog blog;
+	
+	@Reference
+	public void setBlog(Blog blog) {
+		this.blog = blog;
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setHeader("Content-Type", "text/plain");
 		PrintWriter writer = resp.getWriter();
-		writer.println("Hello there");
+		
+		try {
+			for (String entry : blog.listEntries()) {
+				writer.println(entry);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		writer.println("END");
 		writer.flush();
 		writer.close();
 	}
